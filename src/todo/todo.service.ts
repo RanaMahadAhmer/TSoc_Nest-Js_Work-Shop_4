57c4e68fe8f48b95a8bc287be1f19eb7d06861c3
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-08 17:36:37 +0500

    Initial Commit

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { DatabaseService } from '../database/database.service';
+
+@Injectable()
+export class TodoService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createTodoDto: CreateTodoDto) {
+    return;
+  }
+
+  findAll() {
+    return `This action returns all todo`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
